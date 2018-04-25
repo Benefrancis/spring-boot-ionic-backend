53b26e9e98e1b98fb3b6cf9a69f918a6776e2fb6
Benefrancis do Nascimento <benefrancis@gmail.com>
2018-04-25 13:08:14 -0300

    Ajustes no Endpoint Categorias/{id} evitar bug muito para muitos do JPA e criação de exceptions personalizadas para categorias que não existem

--- src/main/java/com/benefrancis/cursomc/services/CategoriaService.java
+++ src/main/java/com/benefrancis/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.benefrancis.cursomc.domain.Categoria;
 import com.benefrancis.cursomc.repositories.CategoriaRepository;
+import com.benefrancis.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,10 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 
