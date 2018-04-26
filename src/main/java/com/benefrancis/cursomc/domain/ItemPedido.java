b35f3738227364f3d395abc15a5c99ff9f8af06b
Benefrancis do Nascimento <benefrancis@gmail.com>
2018-04-26 14:33:15 -0300

    Endpoint /pedido/{id} disponibilizado

--- src/main/java/com/benefrancis/cursomc/domain/ItemPedido.java
+++ src/main/java/com/benefrancis/cursomc/domain/ItemPedido.java
@@ -5,17 +5,17 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class ItemPedido implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-	
-	
+
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
@@ -33,6 +33,7 @@ public class ItemPedido implements Serializable {
 		super();
 	}
 
+	@JsonIgnore
 	public Pedido getPedido() {
 		return this.id.getPedido();
 	}
---
+++ src/main/java/com/benefrancis/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.benefrancis.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.benefrancis.cursomc.domain.Pedido;
+import com.benefrancis.cursomc.repositories.PedidoRepository;
+import com.benefrancis.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+
+}
