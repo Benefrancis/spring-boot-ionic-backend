89222d85619eee1e2289e2751daf762b6ef3357e
Benefrancis do Nascimento <benefrancis@gmail.com>
2018-05-07 13:29:28 -0300

    email Html

--- src/main/java/com/benefrancis/cursomc/services/EmailService.java
+++ src/main/java/com/benefrancis/cursomc/services/EmailService.java
@@ -1,10 +1,17 @@
 package com.benefrancis.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.benefrancis.cursomc.domain.Pedido;
 
 public interface EmailService {
 	public void sendOrderConfirmationEmail(Pedido obj);
-	public void sendEmail(SimpleMailMessage msg);	
+
+	public void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
